bd27b955518fab4bc235b8c0e83641cc3ba6d982
Steven Lian <30307988+stevenlian1994@users.noreply.github.com>
2019-05-24 11:58:51 -0700

    added mongodb and angular

--- server.js
+++ server.js
@@ -3,9 +3,22 @@ var app = express();
 var server = app.listen(8000, function(){
     console.log('Connected to port 8000')
 })
+var mongoose = require('mongoose');
 var io = require('socket.io')(server)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
+
+mongoose.connect('mongodb://localhost/mongoose_dashboard');
+//schemas
+var UserSchema = new mongoose.Schema({
+    first_name: {type: String,},
+    last_name: {type: String,},
+    email: {type: String,},
+})
+//store schemas
+mongoose.model('User', UserSchema)
+//retrieve schema
+var User = mongoose.model('User')
 
 io.on('connection', function(socket) {
     socket.on('message', function(data){
